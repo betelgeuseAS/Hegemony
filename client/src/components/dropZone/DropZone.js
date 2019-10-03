90fc24f6637caa05e663fe896a00a83f38ae562c
Andrew <betelgeuse.as@gmail.com>
2019-10-03 08:36:00 +0300

    drop zone (images & files)

---
+++ client/src/components/dropZone/DropZone.js
@@ -0,0 +1,44 @@
+import React, {Component} from 'react';
+import PropTypes from 'prop-types';
+import Dropzone from 'react-dropzone';
+
+class DropZone extends Component {
+  constructor() {
+    super();
+    this.onDrop = (files) => {
+      this.setState({files})
+    };
+    this.state = {
+      files: []
+    };
+  }
+
+  render() {
+    const files = this.state.files.map(file => (
+      <li key={file.name}>
+        {file.name} - {file.size} bytes
+      </li>
+    ));
+
+    console.log(this.state.files);
+
+    return (
+      <Dropzone onDrop={this.onDrop}>
+        {({getRootProps, getInputProps}) => (
+          <section className="container">
+            <div {...getRootProps({className: 'dropzone'})}>
+              <input {...getInputProps()} />
+              <p>Drag 'n' drop some files here, or click to select files</p>
+            </div>
+            <aside>
+              <h4>Files</h4>
+              <ul>{files}</ul>
+            </aside>
+          </section>
+        )}
+      </Dropzone>
+    );
+  }
+}
+
+export default DropZone;
