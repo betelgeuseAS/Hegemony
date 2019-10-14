78a0f1b9d7a0d2134421b0727b072d220d8f000c
Andrew <betelgeuse.as@gmail.com>
2019-10-14 16:24:09 +0300

    record schema

--- server/models/Record.js
+++ server/models/Record.js
@@ -3,15 +3,32 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const RecordSchema = new Schema({
+  type: {
+    type: String,
+    required: true,
+    default: 'complete'
+  },
   name: {
     type: String,
     required: true
   },
-  phone: {
-    type: String,
-    required: true
+  content: String,
+  tags: {
+    type: [String],
+    default: []
   },
-  address: String
+  location: String,
+  weather: String,
+  files: {
+    type: [String],
+    default: []
+  },
+  created: {
+    type: Date,
+    default: Date.now
+  }
+}, {
+  timestamps: true //Automatically add two new fields - createdAt and updatedAt
 });
 
 module.exports = Record = mongoose.model('records', RecordSchema);
